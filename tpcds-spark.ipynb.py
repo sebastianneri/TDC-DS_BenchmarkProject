f4940a979e4106b0faa6701bdcb9e33ffa284798
sebastianneri <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>
2024-10-20 11:23:56 +0200

    query fix

--- tpcds-spark.ipynb.py
+++ tpcds-spark.ipynb.py
@@ -347,6 +347,6 @@ def run(data_sizes=['1G']):
 # COMMAND ----------
 
 # Please don't run full pipeline unless ready, try with run(data_sizes=['1G'])
-#run(data_sizes=['1G', '2G', '3G', '4G'])
-run(data_sizes=['1G'])
+run(data_sizes=['1G', '2G', '3G', '4G'])
+#run(data_sizes=['1G'])
 
