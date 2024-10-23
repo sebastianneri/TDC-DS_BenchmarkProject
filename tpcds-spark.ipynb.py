9b82374fe154d7062fed9c42b117ce43d07419d0
sebastianneri <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>
2024-10-23 10:55:07 +0200

    query fix

--- tpcds-spark.ipynb.py
+++ tpcds-spark.ipynb.py
@@ -242,7 +242,7 @@ def run_query(run_id, query_number, queries, path_to_save_results, data_size, sp
         # the extra query here should also remove cache
         #execution_plan_filename = f"{query_number}.txt"
         #save_execution_plan(queries[query_number-1], data_size, execution_plan_filename)
-
+        print(queries[query_number-1])
         result = spark.sql(queries[query_number-1])
         count = result.count()
 
